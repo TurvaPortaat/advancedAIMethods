40cc44a8b44e409fe8b8a9d9405f69ea921b1028
KirsiL <117641781+TurvaPortaat@users.noreply.github.com>
2026-02-02 14:33:41 +0200

    Add smoke test and multiple model evaluations in main.py
    Refactor main.py to support smoke testing and training modes with multiple model evaluations.

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 from torchview import draw_graph
-from src.models.inception_style import InceptionCIFAR, Inception_module
 from src.data import get_dataloaders
 from src.train import train_model
+import torch
+from src.models.custom_inception_residual import CustomInceptionResidualNet
+from src.models.custom_residual import CustomResidualNet
+from src.models.small_residual import SmallResidualNet
+from src.models.resnet18_pretrained import ResNet18Pretrained
+from src.models.inception_style import InceptionCIFAR, Inception_module
+
+MODE = "smoke" # "smoke" or "train"
 
 def main():
     trainset, trainloader, valset, valloader, testset, testloader = get_dataloaders(batch_size=64, val_size=5000)
 
     print(len(trainset),len(valset), len(testset))
 
-    model = InceptionCIFAR()
-    train_model(model, trainloader, epochs=1, lr=1e-3)  #lower epochs to 1 during smoketest
+    if MODE == "smoke":
+        x = torch.randn(2, 3, 32, 32)
 
-    print("Train size:", len(trainset)) #smoketest
-    print("Val size:", len(valset))     #smoketest
-    print("Test size:", len(testset))   #smoketest
+        for m in [
+            InceptionCIFAR(),
+            SmallResidualNet(),
+            CustomResidualNet(),
+            ResNet18Pretrained(pretrained=False),
+            CustomInceptionResidualNet(),
+        ]:
+            y = m(x)
+            print(type(m).__name__, y.shape)
+            print("SMOKE TEST DONE – no training executed")
 
-    # VISUALIZATION # Comment during smoketest
+    if MODE == "train":
 
-    #g = draw_graph(Inception_module(), input_size=(1,192,32,32), save_graph=True, filename="inception_block", directory="runs")
+        model = CustomInceptionResidualNet()    # OR ResNetPretrained(), OR InceptionCIFAR(), OR ETC.
+        train_model(model, trainloader, epochs=1, lr=1e-3)  #lower epochs to 1 during smoketest
 
-    #print(g.visual_graph)
+        print("Train size:", len(trainset)) #smoketest
+        print("Val size:", len(valset))     #smoketest
+        print("Test size:", len(testset))   #smoketest
+
+        # VISUALIZATION
+
+        #g = draw_graph(Inception_module(), input_size=(1,192,32,32), save_graph=True, filename="inception_block", directory="runs")
+
+        #print(g.visual_graph)
 
 if __name__ == "__main__":
     main()
