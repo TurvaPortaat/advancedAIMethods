2ad08df1dadef5b73fc414bf5b855caea21b7c5e
KirsiL <117641781+TurvaPortaat@users.noreply.github.com>
2026-02-07 18:06:10 +0200

    Refactor get_dataloaders to include root parameter

--- src/data.py
+++ src/data.py
@@ -5,35 +5,35 @@ from torch.utils.data import DataLoader, random_split
 
 classes = ('plane','car','bird','cat','deer','dog','frog','horse','ship','truck')
 
-def get_dataloaders(batch_size=64, val_size=5000, seed=42, num_workers=2):
+def get_dataloaders(batch_size=64, val_size=5000, seed=42, num_workers=2, root="./data"):
+    train_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.ToTensor(),
+    ])
 
-    train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomCrop(32, padding=4),transforms.ToTensor(),])
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
 
-    test_transform = transforms.Compose([transforms.ToTensor()])
-
-
-    trainset_all_aug = torchvision.datasets.CIFAR10(root='./data', train=True, download = True, transform=train_transform)
-    trainset_all_clean = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=test_transform)
+    # same underlying data, different transforms
+    trainset_all_aug = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=train_transform)
+    trainset_all_clean = torchvision.datasets.CIFAR10(root=root, train=True, download=True, transform=test_transform)
 
     generator = torch.Generator().manual_seed(seed)
-    train_size = len(trainset_all_aug)-val_size
-    train_subset, val_subset_indices = random_split(range(len(trainset_all_aug)), [train_size, val_size], generator=generator)
+    train_size = len(trainset_all_aug) - val_size
+    train_subset, val_subset = random_split(range(len(trainset_all_aug)), [train_size, val_size], generator=generator)
 
     train_indices = list(train_subset)
-    val_indices = list(val_subset_indices)
+    val_indices = list(val_subset)
 
     trainset = torch.utils.data.Subset(trainset_all_aug, train_indices)
-    valset = torch.utils.data.Subset(trainset_all_clean, val_indices)
+    valset   = torch.utils.data.Subset(trainset_all_clean, val_indices)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
+    testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True, transform=test_transform)
 
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True,  num_workers=num_workers)
+    valloader   = DataLoader(valset,   batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    testloader  = DataLoader(testset,  batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-
-    valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-    return  trainset, trainloader, valset, valloader, testset, testloader
-
-
+    return trainset, trainloader, valset, valloader, testset, testloader
